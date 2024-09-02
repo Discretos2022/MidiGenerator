352c327a9995673271dfcf8da1269e0d50546ecd
Gom01 <96956562+Gom01@users.noreply.github.com>
2024-09-02 15:52:45 +0200

    Added Guitar

--- .venv/main.py
+++ .venv/main.py
@@ -4,32 +4,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pretty_midi
 import scipy as sp
-#AUDIO LOADING AND SETTINGS
-audioSample = "res/Interstellar.wav"
+
+
+audioSample = "res/james_bond.mp3"
 HOP_LENGTH = 512
 bins_per_octave = 12
-#sr = 22050
-music, sr = librosa.load(audioSample)
+n_bins = 84
+n_fft = 2048
+sr = 22050
+music, sr = librosa.load(audioSample, sr=sr)
 onset_env = librosa.onset.onset_strength(y=music, sr=sr)
 
-#APPLYING FOURIER AND CREATE SPECTROGRAM
 FMIN = float(librosa.note_to_hz("C1"))
-shortFourier = librosa.cqt(music, fmin=FMIN, bins_per_octave=bins_per_octave)
+shortFourier = librosa.cqt(music, sr=sr, hop_length=HOP_LENGTH, fmin=FMIN, bins_per_octave=bins_per_octave, n_bins=n_bins)
 musicSpectrogram = librosa.amplitude_to_db(np.abs(shortFourier))
-tempo, beat = librosa.beat.beat_track(y=music, sr=sr)
+tempo, beat = librosa.beat.beat_track(y=music, sr=sr, onset_envelope=onset_env)
 print(f"Tempo : {tempo}")
 def displayLinesNotes(musicSpectrogram, sr, hop_length, notes, hline=-1, pr=-1):
-    onset_frames = librosa.onset.onset_detect(y=music, onset_envelope=onset_env, sr=sr)
+    onset_frames = librosa.onset.onset_detect(y=music, onset_envelope=onset_env, sr=sr, hop_length=HOP_LENGTH)
     plt.figure(figsize=(10, 5))
-    plt.vlines(librosa.frames_to_time(onset_frames), 0, 8192, color="b")
+    plt.vlines(librosa.frames_to_time(onset_frames, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft), 0, 15000, color="b")
     if pr != -1:
-        plt.vlines(librosa.frames_to_time(onset_frames) + librosa.frames_to_time(pr), 0, 8192, color="g")
-        plt.vlines(librosa.frames_to_time(onset_frames) - librosa.frames_to_time(pr), 0, 8192, color="g")
+        plt.vlines(librosa.frames_to_time(onset_frames, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft) + librosa.frames_to_time(pr, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft), 0, 15000, color="g")
+        plt.vlines(librosa.frames_to_time(onset_frames, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft) - librosa.frames_to_time(pr, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft), 0, 15000, color="g")
     if hline != -1:
-        plt.hlines(hline, 0, 12, color="g")
+        plt.hlines(hline, 0, 100, color="g")
     librosa.display.specshow(musicSpectrogram,
                              sr=sr,
-                             hop_length=hop_length,
+                             hop_length=HOP_LENGTH,
+                             n_fft=n_fft,
                              x_axis="time",
                              y_axis="cqt_hz",
                              bins_per_octave=bins_per_octave,
@@ -40,12 +43,12 @@ def displayLinesNotes(musicSpectrogram, sr, hop_length, notes, hline=-1, pr=-1):
     plt.colorbar(format="%+2.f dB")
 
 def hz_to_cqt(hz):
-    frequencies = librosa.cqt_frequencies(84,fmin=FMIN, bins_per_octave=bins_per_octave).tolist()
+    frequencies = librosa.cqt_frequencies(n_bins,fmin=FMIN, bins_per_octave=bins_per_octave).tolist()
     return frequencies.index(hz)
 
 def searchMaxValues(graph):
     threshold = -30
-    frequencies = librosa.cqt_frequencies(84, fmin=FMIN, bins_per_octave=bins_per_octave)
+    frequencies = librosa.cqt_frequencies(n_bins, fmin=FMIN, bins_per_octave=bins_per_octave)
     #going through all the value in one frame
     values = []
     for value in range(0,len(graph)-1):
@@ -60,7 +63,7 @@ def searchMaxValues(graph):
 def dataToDisplayNotes(table):
     displayable = []
     for note in range(len(table)):
-        time = librosa.frames_to_time(table[note][1])
+        time = librosa.frames_to_time(table[note][1], sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft)
         frequency = table[note][0]
         displayable.append([time,frequency])
     return(displayable)
@@ -90,7 +93,7 @@ def isFondamentale(value, hz):
 def searchNote():
     notes = []
     #go through all the frames
-    onset_frames = librosa.onset.onset_detect(y=music, onset_envelope=onset_env, sr=sr)
+    onset_frames = librosa.onset.onset_detect(y=music, onset_envelope=onset_env, sr=sr, hop_length=HOP_LENGTH)
     for i,frame in enumerate(onset_frames):
         #change the orientation of the table
         tableTransposed = np.transpose(musicSpectrogram)
@@ -135,7 +138,7 @@ def getMaxiFreq(values):
 
 def getDuration(notes, spectrogram):
     #frame,frequencies
-    frequencies = (librosa.cqt_frequencies(84,fmin=FMIN, bins_per_octave=bins_per_octave)).tolist()
+    frequencies = (librosa.cqt_frequencies(n_bins,fmin=FMIN, bins_per_octave=bins_per_octave)).tolist()
     finalnotes = []
     for note in range(len(notes)):
         frequency = notes[note][1]
@@ -147,39 +150,45 @@ def getDuration(notes, spectrogram):
             currentDecibel = spectrogram[row][frame]
             if startingDecibel > 0:
                 if (startingDecibel - (startingDecibel*.5)) > currentDecibel:
-                    if(frame-startFrame) < 40:
+                    if(frame-startFrame) > 15 and (frame-startFrame) < 600:
                         finalnotes.append([frequency, startFrame, frame, frame-startFrame])
                         print(f"freq {frequency}, start {startFrame}, stop {frame}, total {frame-(startFrame)}")
                         break
             else:
                 if startingDecibel - abs(startingDecibel*.5) > currentDecibel:
-                    if(frame-startFrame) < 40:
+                    if(frame-startFrame) > 15 and (frame-startFrame) < 600:
                         finalnotes.append([frequency, startFrame, frame, frame - startFrame])
                         print(f"freq {frequency}, start {startFrame}, stop {frame}, total {frame - (startFrame)}")
                         break
     return(finalnotes)
 
 ##6 Midi Conversion
-def midiConversion(notes):
-    piano_Instrument = pretty_midi.PrettyMIDI(initial_tempo=round(tempo[0]))
-    piano = pretty_midi.Instrument(program=0)
+def midiConversion(notes, instrument):
+    instru = 0
+    if instrument == "piano":
+        instru = pretty_midi.Instrument(program=0)
+        print("Song Converted Piano")
+    else:
+        instru = pretty_midi.Instrument(program=25)
+        print("Song Converted Guitare")
+    file = pretty_midi.PrettyMIDI(initial_tempo=round(tempo[0]))
     for note in range(len(notes)):
         frequency = notes[note][0]
         startFrame = notes[note][1]
         stopFrame = notes[note][2]
         note_number = round(pretty_midi.hz_to_note_number(float(frequency)))
-        startTime = float(librosa.frames_to_time(startFrame))
-        stopTime = float(librosa.frames_to_time(stopFrame))
+        startTime = float(librosa.frames_to_time(startFrame, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft))
+        stopTime = float(librosa.frames_to_time(stopFrame, sr=sr, hop_length=HOP_LENGTH, n_fft=n_fft))
 
         note = pretty_midi.Note(velocity=45, pitch=note_number, start=startTime, end=stopTime)
-        piano.notes.append(note)
+        instru.notes.append(note)
+    file.instruments.append(instru)
+    file.write('FINAL_MUSIC.mid')
 
-    piano_Instrument.instruments.append(piano)
-    piano_Instrument.write('pianoTEST.mid')
 
 #MAIN
 tableOfNotes = searchNote()
 music = getDuration(tableOfNotes,musicSpectrogram)
 displayLinesNotes(musicSpectrogram,sr,HOP_LENGTH, dataToDisplayNotes(music))
-midiConversion(music)
+midiConversion(music, "guitare")
 plt.show()
